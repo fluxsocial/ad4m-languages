ed91ececf2a7c371b54f15d446c6e88d2971598e
Kaichao Sun <kaichaosuna@gmail.com>
2021-10-18 12:06:51 +0800

    migrate group expression

--- release.sh
+++ release.sh
@@ -11,7 +11,7 @@ echo "Creating three releases of languages inside ./release"
 
 [ ! -d "./release" ] && mkdir "./release"
 
-# SDP expression release
+################### SDP expression release ###################
 echo "Create SDP expression release..."
 
 # Get new dna.yaml with correct props & build language
@@ -28,7 +28,7 @@ cd ..
 cp ./sdp-expression/build/bundle.js ./release/sdp/bundle.js
 cp ./hc-dna/workdir/sdp-expression.dna ./release/sdp/sdp-expression.dna
 
-# IceCandidate expression release
+################### IceCandidate expression release ###################
 echo "Create IceCandidate expression release..."
 
 # Get new dna.yaml with correct props & build language
@@ -45,7 +45,7 @@ cd ..
 cp ./icecandidate-expression/build/bundle.js ./release/icecandidate/bundle.js
 cp ./hc-dna/workdir/icecandidate-expression.dna ./release/icecandidate/icecandidate-expression.dna
 
-# ShortForm expression release begin
+################### ShortForm expression release begin ###################
 echo "Create ShortForm expression release..."
 
 # Get new dna.yaml with correct props & build language
@@ -62,6 +62,23 @@ npm install && npm run build-nix
 cp ./build/bundle.js ./release/shortform/bundle.js
 cp ./hc-dna/workdir/shortform-expression.dna ./release/shortform/shortform-expression.dna
 
-# Finish release process, move original files back
+################### Group expression release begin ###################
+echo "Create Group expression release..."
+
+# Get new dna.yaml with correct props & build language
+[ ! -e "./hc-dna/workdir/dna_origin.yaml" ] && cp ./hc-dna/workdir/dna.yaml ./hc-dna/workdir/dna_origin.yaml
+[ ! -e "./dna_origin.js" ] && cp ./dna.js ./dna_origin.js
+cp ./group-expression/dna.yaml ./hc-dna/workdir/dna.yaml
+cp ./group-expression/dna.js ./dna.js
+npm install && npm run build-nix
+
+# Check if release/group directory exists, if not create one
+[ ! -d "./release/group" ] && mkdir -p "./release/group"
+
+# Copy the build files to the release dir
+cp ./build/bundle.js ./release/group/bundle.js
+cp ./hc-dna/workdir/group-expression.dna ./release/group/group-expression.dna
+
+################### Finish release process, move original files back ###################
 mv ./hc-dna/workdir/dna_origin.yaml ./hc-dna/workdir/dna.yaml
 mv ./dna_origin.js ./dna.js
