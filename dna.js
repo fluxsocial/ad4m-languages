3f5c98b3dc4c7bd8a27a2e96ea62ddbd00cb2631
Kaicho Sun <kaichaosuna@gmail.com>
2021-11-30 10:53:41 +0800

    rename language follow convention

--- dna.js
+++ dna.js
@@ -3,6 +3,6 @@ import dna from "./hc-dna/workdir/demo-expression.dna";
 export const DNA = Buffer.from(dna, 'base64');
 
 export const CONFIG = {
-    "languageName": "junto-demo",
+    "languageName": "demo-expression",
     "dnaName": "demo-expression"
 };
--- shortform-expression/dna.js
+++ shortform-expression/dna.js
@@ -3,6 +3,6 @@ import dna from "./hc-dna/workdir/shortform-expression.dna";
 export const DNA = Buffer.from(dna, 'base64');
 
 export const CONFIG = {
-    "languageName": "junto-shortform",
+    "languageName": "shortform-expression",
     "dnaName": "shortform-expression"
 };
